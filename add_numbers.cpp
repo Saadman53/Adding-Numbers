58f88f95a0b550e6982ecae4187e283feb74aba1
Saadman Sakib <54435620+Saadman53@users.noreply.github.com>
2023-07-08 11:26:52 +0600

    add add_numbers.cpp
    Created a function called add that takes two integers as parameters and returns their sum.

--- add_numbers.cpp
+++ add_numbers.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 
+int add(int n1, int n2){
+	return n1+n2;
+}
 int main(){
 	int num1, num2;
 	cin>>num1>>num2;
-	cout<<num1+num2;
+	cout<<add(num1,num2);
 }
